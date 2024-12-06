0dfd57ed863afae65e29d50230a14f1ac8f7f03e
Akibur Rahman <akibur.choton@gmail.com>
2024-12-06 22:44:50 +1100

    Initial commit: Set up project structure and configure TypeScript environment

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { fetchData } from './modules';
+
+
+const data = fetchData();
+console.log('Fetched Data:', data);
---
+++ src/modules.ts
@@ -0,0 +1,8 @@
+import data from '../data.json';
+
+
+
+// Function to fetch the JSON data
+export const fetchData = () => {
+  return data;
+};
